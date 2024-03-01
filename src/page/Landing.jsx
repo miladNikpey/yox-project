175fcdbdda946bdc39894c93d83ef2bb4e6675c3
Milad <miladnik5040@gmial.com>
2024-03-01 14:28:20 +0330

    some changes on landing page

--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Header from '../components/header/Header'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Header from "../components/header/Header";
+import HeadSlider from "../widget/headSlider/HeadSlider";
+import FeatureBox from "../widget/featureBox/FeatureBox";
+import coverF from "../assets/images/banFirst.svg";
+import coverS from "../assets/images/banSecond.svg";
+import style from "./landing.module.scss"
 
 const Landing = () => {
   return (
-    <div>
-        <Navbar/>
-        <Header/>
+    <div className={style.landingWrapper}>
+      <Navbar />
+      <Header />
+      <HeadSlider />
+      <FeatureBox />
+      <div className={style.coverNews}>
+        <div>
+          <img className={style.coverNewsImg} src={coverF} alt="" />
+        </div>
+        <div>
+          <img className={style.coverNewsImg} src={coverS} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
